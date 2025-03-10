a22246288b9a31a8e456de76bf8d8fc1d3d9f2f9
Rishabh Goyal <rishabhgoyal121@gmail.com>
2025-03-10 21:15:40 +0530

    feat: add axios api calls for movies page and its sub-pages

--- src/app/movies/mostPopular/page.tsx
+++ src/app/movies/mostPopular/page.tsx
@@ -1,7 +1,100 @@
-export default function MostPopularMovies() {
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const mostPopularMoviesUrl =
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+const options = {
+  method: "GET",
+  url: mostPopularMoviesUrl,
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMDNmN2FiYTM0OTFhYjU4ZTdmNjRlMmMzMTQ1YjA2MSIsIm5iZiI6MTc0MTM1ODYxNy4wNCwic3ViIjoiNjdjYjA2MTk4MWZiYjEyNTM5Y2I2Yzk4Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.cnt2SO-eBt71o4iBF5c26AwHDXwJF4ND5ZhQGaQbnJM",
+  },
+};
+export default function Page() {  
+  const [loadingMostPopularMovies, setLoadingMostPopularMovies] =
+    useState(true);
+  const [mostPopularMovies, setMostPopularMovies] = useState([
+    {
+      adult: false,
+      backdrop_path: "/9nhjGaFLKtddDPtPaX5EmKqsWdH.jpg",
+      genre_ids: [10749, 878, 53],
+      id: 950396,
+      original_language: "en",
+      original_title: "The Gorge",
+      overview:
+        "Two highly trained operatives grow close from a distance after being sent to guard opposite sides of a mysterious gorge. When an evil below emerges, they must work together to survive what lies within.",
+      popularity: 350.678,
+      poster_path: "/7iMBZzVZtG0oBug4TfqDb9ZxAOa.jpg",
+      release_date: "2025-02-13",
+      title: "The Gorge",
+      video: false,
+      vote_average: 7.775,
+      vote_count: 1765,
+    },
+    {
+      adult: false,
+      backdrop_path: "/ek8CJRZchT9YIB4p7ktEjPXuCIi.jpg",
+      genre_ids: [28, 53, 80],
+      id: 1126166,
+      original_language: "en",
+      original_title: "Flight Risk",
+      overview:
+        "A U.S. Marshal escorts a government witness to trial after he's accused of getting involved with a mob boss, only to discover that the pilot who is transporting them is also a hitman sent to assassinate the informant. After they subdue him, they're forced to fly together after discovering that there are others attempting to eliminate them.",
+      popularity: 333.328,
+      poster_path: "/q0bCG4NX32iIEsRFZqRtuvzNCyZ.jpg",
+      release_date: "2025-01-22",
+      title: "Flight Risk",
+      video: false,
+      vote_average: 6.064,
+      vote_count: 414,
+    },
+    {
+      adult: false,
+      backdrop_path: "/kEYWal656zP5Q2Tohm91aw6orlT.jpg",
+      genre_ids: [18, 35, 10749],
+      id: 1064213,
+      original_language: "en",
+      original_title: "Anora",
+      overview:
+        "A young sex worker from Brooklyn gets her chance at a Cinderella story when she meets and impulsively marries the son of an oligarch. Once the news reaches Russia, her fairytale is threatened as his parents set out to get the marriage annulled.",
+      popularity: 269.054,
+      poster_path: "/qh8m8Udz0sCa5gy9VaqfHPh0yPM.jpg",
+      release_date: "2024-10-14",
+      title: "Anora",
+      video: false,
+      vote_average: 7.1,
+      vote_count: 1553,
+    },
+  ]);
+  const [errorMostPopularMovies, setErrorMostPopularMovies] = useState(null);
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then((res) => {
+        console.log(res.data);
+        setMostPopularMovies(res.data.results);
+        setLoadingMostPopularMovies(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoadingMostPopularMovies(false);
+        setErrorMostPopularMovies(err.message);
+      });
+  }, []);
+
   return (
     <>
-      <p>Most Popular Movies</p>
+      <h2>Most Popular Movies</h2>
+      {loadingMostPopularMovies && <p>Loading...</p>}
+      {mostPopularMovies.length > 0 &&
+        mostPopularMovies.map((movie) => {
+          return <li key={movie.id}>{movie.original_title}</li>;
+        })}
+      {errorMostPopularMovies && <p>{errorMostPopularMovies}</p>}
     </>
   );
 }
--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,7 +1,100 @@
-export default function Movies() {
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const upcomingMoviesUrl =
+  "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1";
+
+const options = {
+  method: "GET",
+  url: upcomingMoviesUrl,
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMDNmN2FiYTM0OTFhYjU4ZTdmNjRlMmMzMTQ1YjA2MSIsIm5iZiI6MTc0MTM1ODYxNy4wNCwic3ViIjoiNjdjYjA2MTk4MWZiYjEyNTM5Y2I2Yzk4Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.cnt2SO-eBt71o4iBF5c26AwHDXwJF4ND5ZhQGaQbnJM",
+  },
+};
+export default function Page() {
+  const [loading, setLoading] = useState(true);
+  const [upcomingMovies, setUpcomingMovies] = useState([
+    {
+      adult: false,
+      backdrop_path: "/ek8CJRZchT9YIB4p7ktEjPXuCIi.jpg",
+      genre_ids: [28, 53, 80],
+      id: 1126166,
+      original_language: "en",
+      original_title: "Flight Risk",
+      overview:
+        "A U.S. Marshal escorts a government witness to trial after he's accused of getting involved with a mob boss, only to discover that the pilot who is transporting them is also a hitman sent to assassinate the informant. After they subdue him, they're forced to fly together after discovering that there are others attempting to eliminate them.",
+      popularity: 333.328,
+      poster_path: "/q0bCG4NX32iIEsRFZqRtuvzNCyZ.jpg",
+      release_date: "2025-01-22",
+      title: "Flight Risk",
+      video: false,
+      vote_average: 6.1,
+      vote_count: 414,
+    },
+    {
+      adult: false,
+      backdrop_path: "/sc1abgWNXc29wSBaerrjGBih06l.jpg",
+      genre_ids: [27, 878, 53],
+      id: 1084199,
+      original_language: "en",
+      original_title: "Companion",
+      overview:
+        "During a weekend getaway at a secluded lakeside estate, a group of friends finds themselves entangled in a web of secrets, deception, and advanced technology. As tensions rise and loyalties are tested, they uncover unsettling truths about themselves and the world around them.",
+      popularity: 128.04,
+      poster_path: "/oCoTgC3UyWGfyQ9thE10ulWR7bn.jpg",
+      release_date: "2025-01-22",
+      title: "Companion",
+      video: false,
+      vote_average: 7,
+      vote_count: 710,
+    },
+    {
+      adult: false,
+      backdrop_path: "/b3mdmjYTEL70j7nuXATUAD9qgu4.jpg",
+      genre_ids: [16, 14, 12],
+      id: 823219,
+      original_language: "lv",
+      original_title: "Straume",
+      overview:
+        "A solitary cat, displaced by a great flood, finds refuge on a boat with various species and must navigate the challenges of adapting to a transformed world together.",
+      popularity: 173.824,
+      poster_path: "/imKSymKBK7o73sajciEmndJoVkR.jpg",
+      release_date: "2024-08-29",
+      title: "Flow",
+      video: false,
+      vote_average: 8.3,
+      vote_count: 1432,
+    },
+  ]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then((res) => {
+        console.log(res.data);
+        setUpcomingMovies(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("err", err);
+        setLoading(false);
+        setError(err.message);
+      });
+  }, []);
+
   return (
     <>
-      <p>Movies</p>
+      <h2>Upcoming Movies</h2>
+      {loading && <p>Loading...</p>}
+      {upcomingMovies.length > 0 &&
+        upcomingMovies.map((movie) => {
+          return <li key={movie.id}>{movie.original_title}</li>;
+        })}
+      {error && <p>{error}</p>}
     </>
   );
 }
