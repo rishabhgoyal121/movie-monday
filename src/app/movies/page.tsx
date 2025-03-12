d26ff41d1ba8ae5ebc557e8f20eec2ba8366c278
Rishabh Goyal <rishabhgoyal121@gmail.com>
2025-03-12 19:19:17 +0530

    feat: add rating input slider functionality to (upcoming) movies page

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -79,11 +79,65 @@ export default function Page() {
       vote_count: 1432,
     },
   ]);
+  const [filteredUpcomingMovies, setFilteredUpcomingMovies] = useState([
+    {
+      adult: false,
+      backdrop_path: "/ek8CJRZchT9YIB4p7ktEjPXuCIi.jpg",
+      genre_ids: [28, 53, 80],
+      id: 1126166,
+      original_language: "en",
+      title: "Flight Risk",
+      overview:
+        "A U.S. Marshal escorts a government witness to trial after he's accused of getting involved with a mob boss, only to discover that the pilot who is transporting them is also a hitman sent to assassinate the informant. After they subdue him, they're forced to fly together after discovering that there are others attempting to eliminate them.",
+      popularity: 333.328,
+      poster_path: "/q0bCG4NX32iIEsRFZqRtuvzNCyZ.jpg",
+      release_date: "2025-01-22",
+      original_title: "Flight Risk",
+      video: false,
+      vote_average: 6.1,
+      vote_count: 414,
+    },
+    {
+      adult: false,
+      backdrop_path: "/sc1abgWNXc29wSBaerrjGBih06l.jpg",
+      genre_ids: [27, 878, 53],
+      id: 1084199,
+      original_language: "en",
+      title: "Companion",
+      overview:
+        "During a weekend getaway at a secluded lakeside estate, a group of friends finds themselves entangled in a web of secrets, deception, and advanced technology. As tensions rise and loyalties are tested, they uncover unsettling truths about themselves and the world around them.",
+      popularity: 128.04,
+      poster_path: "/oCoTgC3UyWGfyQ9thE10ulWR7bn.jpg",
+      release_date: "2025-01-22",
+      original_title: "Companion",
+      video: false,
+      vote_average: 7,
+      vote_count: 710,
+    },
+    {
+      adult: false,
+      backdrop_path: "/b3mdmjYTEL70j7nuXATUAD9qgu4.jpg",
+      genre_ids: [16, 14, 12],
+      id: 823219,
+      original_language: "lv",
+      original_title: "Straume",
+      overview:
+        "A solitary cat, displaced by a great flood, finds refuge on a boat with various species and must navigate the challenges of adapting to a transformed world together.",
+      popularity: 173.824,
+      poster_path: "/imKSymKBK7o73sajciEmndJoVkR.jpg",
+      release_date: "2024-08-29",
+      title: "Flow",
+      video: false,
+      vote_average: 8.3,
+      vote_count: 1432,
+    },
+  ]);
   const [error, setError] = useState(null);
   const [filters, setFilters] = useState({
     vote_average: 0,
   });
 
+  // effect for fetch call on component initial render
   useEffect(() => {
     axios
       .request(options)
@@ -97,7 +151,16 @@ export default function Page() {
         setLoading(false);
         setError(err.message);
       });
-  });
+  }, []);
+
+  // effect for filtering data when filter value changes
+  useEffect(() => {
+    const newFilteredUpcomingMovies = upcomingMovies.filter((movie) => { return movie.vote_average > filters.vote_average })
+    console.log("newFilteredUpcomingMovies", newFilteredUpcomingMovies);
+    
+    setFilteredUpcomingMovies(newFilteredUpcomingMovies);
+  },[filters, upcomingMovies])
+  
 
   return (
     <>
@@ -131,8 +194,8 @@ export default function Page() {
           </Accordion>
         </div>
         <div className="w-[80vw] flex flex-wrap">
-          {upcomingMovies.length > 0 &&
-            upcomingMovies.map((movie) => {
+          {filteredUpcomingMovies.length > 0 &&
+            filteredUpcomingMovies.map((movie) => {
               return (
                 <div className="flex px-2 py-8 w-44" key={movie.id}>
                   <Card>
