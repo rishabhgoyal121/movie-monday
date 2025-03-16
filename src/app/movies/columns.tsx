55fb1489e41a053155c07b8a93b0d72bb1be19d2
Rishabh Goyal <rishabhgoyal121@gmail.com>
2025-03-16 11:08:06 +0530

    feat: added context menu to rows in table component on movies home page.

--- src/app/movies/columns.tsx
+++ src/app/movies/columns.tsx
@@ -4,6 +4,15 @@ import { ColumnDef } from "@tanstack/react-table";
 import type { Movie } from "@/interfaces/movies";
 import Image from "next/image";
 import Link from "next/link";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const columns: ColumnDef<Movie>[] = [
   {
@@ -56,5 +65,33 @@ export const columns: ColumnDef<Movie>[] = [
   {
     accessorKey: "release_date",
     header: "Release Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="bg-[#0060d199]">
+            <DropdownMenuItem
+              onClick={() =>
+                navigator.clipboard.writeText(`${row.original.id}`)
+              }
+            >
+              Copy movie ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href={`/movies/${row.original.id}`}>View Movie Page</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
