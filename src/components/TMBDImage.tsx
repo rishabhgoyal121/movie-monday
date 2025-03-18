24281b9f911b6ae87da4cda622ef57980df8e5b9
Rishabh Goyal <rishabhgoyal121@gmail.com>
2025-03-18 21:44:35 +0530

    fix : handle missing src case in TMBDImage component

--- src/components/TMBDImage.tsx
+++ src/components/TMBDImage.tsx
@@ -1,19 +1,24 @@
 import Image from "next/image";
 
 interface TMBDImageProps {
-  src: string | null|undefined;
+  src: string | null | undefined;
   alt: string;
 }
 
-export default function TMBDImage({ src, alt='' }: TMBDImageProps) {
-  return (
-    <Image
-      src={`https://image.tmdb.org/t/p/original/${src}`}
-      alt={alt}
-      height={160}
-      width={90}
-      layout="responsive"
-      className="rounded-xl"
-    />
-  );
+export default function TMBDImage({ src, alt = "" }: TMBDImageProps) {
+  if (src) {
+    return (
+      <>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${src}`}
+          alt={alt}
+          height={160}
+          width={90}
+          layout="responsive"
+          className="rounded-xl"
+        />
+      </>
+    );
+  }
+  return <p className="font-semibold text-center h-full">{alt}</p>;
 }
