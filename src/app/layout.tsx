4530b9f605e9ece1645d59b4d9ef39997eb47e2e
Rishabh Goyal <rishabhgoyal121@gmail.com>
2025-03-19 10:22:36 +0530

    feat: Add ScrollToTop functionality on each page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import ScrollToTop from "@/components/ScollToTop";
 
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {" "}
+        <ScrollToTop />
         <Navbar />
         {children}
       </body>
